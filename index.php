16402220bc9f854b4bb287ba101cfbe06f5246fc
ghlee <rjs993530@gmail.com>
2022-02-08 10:55:01 +0900

    2022 0208 10:54

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+
+  <style>
+  </style>
+  
+  <body>
+        <?php
+            echo ("Hello World!!! <br>");
+        ?>
+  </body>
+</html>
+
